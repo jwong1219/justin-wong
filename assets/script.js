0bcb96fa9ac73bdc1009775d35e4388f2a372f4c
jwong1219 <jwong1219@gmail.com>
2017-11-17 23:40:14 -0800

    have a favicon and autoscrolling links. Working on overlay scripts

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,26 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+$(function() {
+  $(".jumper").on("click", function(event) {
+      
+    event.preventDefault();
+
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);    
+  });
+
+  $(".project").hover(function() {
+    let overlay = `#overlay-${$(this).data("overlay")}`;
+    console.log(overlay);
+    console.log(`${overlay}`)
+    $(`${overlay}`).style.display="block";
+  },
+    function() {
+      let overlay = `#overlay-${$(this).data("overlay")}`;
+      console.log(overlay);
+      $(`${overlay}`).style.display="none";
+  })
+})
